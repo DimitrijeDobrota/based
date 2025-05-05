c521dbe99b9c3fe2f43691c3d2fac4ea01d3883a
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2025-05-05 13:49:29 +0200

    Proper header-only config

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -6,6 +6,9 @@ if(PROJECT_IS_TOP_LEVEL)
   set_property(CACHE CMAKE_INSTALL_INCLUDEDIR PROPERTY TYPE PATH)
 endif()
 
+# Project is configured with no languages, so tell GNUInstallDirs the lib dir
+set(CMAKE_INSTALL_LIBDIR lib CACHE PATH "")
+
 include(CMakePackageConfigHelpers)
 include(GNUInstallDirs)
 
@@ -13,9 +16,7 @@ include(GNUInstallDirs)
 set(package based)
 
 install(
-    DIRECTORY
-    include/
-    "${PROJECT_BINARY_DIR}/export/"
+    DIRECTORY include/
     DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
     COMPONENT based_Development
 )
@@ -23,25 +24,18 @@ install(
 install(
     TARGETS based_based
     EXPORT basedTargets
-    RUNTIME #
-    COMPONENT based_Runtime
-    LIBRARY #
-    COMPONENT based_Runtime
-    NAMELINK_COMPONENT based_Development
-    ARCHIVE #
-    COMPONENT based_Development
-    INCLUDES #
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+    INCLUDES DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
 )
 
 write_basic_package_version_file(
     "${package}ConfigVersion.cmake"
     COMPATIBILITY SameMajorVersion
+    ARCH_INDEPENDENT
 )
 
 # Allow package maintainers to freely override the path for the configs
 set(
-    based_INSTALL_CMAKEDIR "${CMAKE_INSTALL_LIBDIR}/cmake/${package}"
+    based_INSTALL_CMAKEDIR "${CMAKE_INSTALL_DATADIR}/${package}"
     CACHE STRING "CMake package config location relative to the install prefix"
 )
 set_property(CACHE based_INSTALL_CMAKEDIR PROPERTY TYPE PATH)
